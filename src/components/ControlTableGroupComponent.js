948faa19cb14d6057f636087881bacaff23b9b93
hengl <tenn_luo@hotmail.com>
2017-05-22 23:05:45 +0200

    add control button style

--- src/components/ControlTableGroupComponent.js
+++ src/components/ControlTableGroupComponent.js
@@ -3,11 +3,33 @@
  */
 
 import React from 'react'
+import styled from 'styled-components'
+
+const Il = styled.i`
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    transform: rotate(135deg);
+    -webkit-transform: rotate(135deg);
+`
+
+const Ir = styled.i`
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    display: inline-block;
+    padding: 3px;
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+`
+
+
+
 const ControlTableGroup = (props) => {
     return(
         <div>
-            <button onClick={()=>props.gotopreviousweek()}>previous</button>
-            <button onClick={()=>props.gotonextweek()}>next</button>
+            <button onClick={()=>props.gotopreviousweek()}><Il />previous</button>
+            <button onClick={()=>props.gotonextweek()}>next<Ir /></button>
         </div>
             )
 }
